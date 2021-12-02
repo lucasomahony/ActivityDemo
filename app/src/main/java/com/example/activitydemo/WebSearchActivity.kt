8bd6a2fd7767e57b50c4615e8d496bf91449c529
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-02 22:59:01 +0000

    Getting the WebView to load the URL

--- app/src/main/java/com/example/activitydemo/WebSearchActivity.kt
+++ app/src/main/java/com/example/activitydemo/WebSearchActivity.kt
@@ -2,6 +2,7 @@ package com.example.activitydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
 import com.example.activitydemo.databinding.ActivityWebSearchBinding
 
 class WebSearchActivity : AppCompatActivity() {
@@ -15,5 +16,10 @@ class WebSearchActivity : AppCompatActivity() {
         val view = binding.root
 
         setContentView(view)
+        binding.webView.webViewClient = WebViewClient() //prevents opening in browser app
+        binding.webView.settings.javaScriptEnabled = true // required for search functionality on Kotlin and Android sites
+
+        val url = intent.getStringExtra("url")
+        binding.webView.loadUrl(url!!)
     }
 }
