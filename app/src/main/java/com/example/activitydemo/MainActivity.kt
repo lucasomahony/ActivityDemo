c4c4e875b594420ed6836458bae171021553be91
lucasomahony <93211681+lucasomahony@users.noreply.github.com>
2021-12-02 22:42:04 +0000

    Transitioning to the Activity with WebView

--- app/src/main/java/com/example/activitydemo/MainActivity.kt
+++ app/src/main/java/com/example/activitydemo/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.activitydemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.activitydemo.databinding.ActivityMainBinding
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val encodedSearchTerm : String
+        get() = URLEncoder.encode(binding.etSearch.text.toString(),"UTF-8")
 
+    private fun loadWebActivity(url: String) {
+        val intent = Intent(this, WebSearchActivity::class.java)
+        intent.putExtra("url", url)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +23,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnGoogle.setOnClickListener {
+            loadWebActivity("https://google.co.uk/search?q=$encodedSearchTerm")
+        }
+        binding.btnStackOverflow.setOnClickListener {
+            loadWebActivity("https://stackoverflow.com/search?q=$encodedSearchTerm")
+        }
+        binding.btnKotlin.setOnClickListener{
+            loadWebActivity("https://kotlinlang.org/?q=$encodedSearchTerm&p=0")
+        }
+        binding.btnAndroid.setOnClickListener{
+            loadWebActivity("https://developer.android.com/s/results/?q=$encodedSearchTerm")
+        }
     }
 }
